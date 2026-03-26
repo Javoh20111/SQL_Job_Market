68a0a5cd4b04d821c4c0ec9093863322abd50e39
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-03-26 11:30:50 +0500

    Refactor SQL query to retrieve top skills for highest-paying Data Analyst jobs

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -5,21 +5,29 @@ Question: What skills are required for the top-paying data analyst jobs?
 -Why? It provides a detailed look at which high-paying jobs demand cerain skills,
     helping job seekers underdstand which skills to develop which aligns with top salaries 
  */
+WITH top_10_highest_paying_jobs AS(
+    SELECT 
+        job_id,
+        job_title,
+        salary_year_avg
+    FROM
+        job_postings_fact
+    LEFT JOIN company_dim ON
+        company_dim.company_id = job_postings_fact.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
+    ORDER BY salary_year_avg DESC
+    LIMIT 10
+)
 
-SELECT 
-    job_id,
-    company_dim.name AS company_name,
-    job_title,
-    job_location,
-    job_schedule_type,
-    salary_year_avg,
-    job_posted_date
-FROM
-    job_postings_fact
-LEFT JOIN company_dim ON
-    company_dim.company_id = job_postings_fact.company_id
-WHERE
-    job_title_short = 'Data Analyst' AND
-    job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
-ORDER BY salary_year_avg DESC
-LIMIT 10;
+SELECT DISTINCT skills_dim.skills, 
+    COUNT(skills_dim.skills) AS popularity
+FROM top_10_highest_paying_jobs
+LEFT JOIN skills_job_dim ON
+    top_10_highest_paying_jobs.job_id = skills_job_dim.job_id
+LEFT JOIN skills_dim ON
+    skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY skills_dim.skills
+ORDER BY popularity DESC
+LIMIT 5;
