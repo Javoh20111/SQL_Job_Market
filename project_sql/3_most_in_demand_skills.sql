8c56e2cf5660a8e5fe10b10333c28f8a06ae6396
Javohir Eshonov <eshonovjavohir92@gmail.com>
2026-03-26 17:12:12 +0500

    Add SQL script to identify most in-demand skills for data analysts

---
+++ project_sql/3_most_in_demand_skills.sql
@@ -0,0 +1,9 @@
+/* 
+Question: What are the most in-demand skills for data analyst?
+-Join job postings to inner join query similarly to query 2
+-Identify the top 5 skills for a data analyst
+- Focus on all job postings.
+- Why? Provide insights to job seekers
+ */
+
+ 
